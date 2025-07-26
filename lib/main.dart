44ee54a20806d7cdd424a399a8d2dec44291b677
StretchWave <78847401+StretchWave@users.noreply.github.com>
2025-07-26 13:43:39 +0530

    sad

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LevelUpFitness',
+      title: 'LevelUp Fitness',
       theme: ThemeData(
         // This is the theme of your application.
         //
